a83892bd0f9e5c39af9ee26c4b58e1f5bfb88ff1
Adrian L Lange <git@p3lim.net>
2024-12-19 03:02:47 +0100

    Screw it

--- action.py
+++ action.py
@@ -48,7 +48,7 @@ body:
 INPUT_TEMPLATE = '''
   - type: input
     attributes:
-      label: '"{}"'
+      label: {}
       placeholder: Translation here
     validations:
       required: true
@@ -91,9 +91,8 @@ elif sys.argv[1] == 'template':
         match = STRING_PATTERN.match(line)
         for match in STRING_PATTERN.finditer(line):
           for group in match.groups():
-            string = group.replace("'", r"\'")
-            if not string in strings:
-              strings.append(string)
+            if not group in strings:
+              strings.append(group)
 
   inputs = []
   for s in sorted(strings):
